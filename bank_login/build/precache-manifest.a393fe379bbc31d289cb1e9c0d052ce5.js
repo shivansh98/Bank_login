b46fd3d105287d2f2c138687a8b5ef7ccb463c0e
shivansh98 <66006609+shivansh98@users.noreply.github.com>
2020-05-27 15:26:52 +0530

    Add files via upload

---
+++ bank_login/build/precache-manifest.a393fe379bbc31d289cb1e9c0d052ce5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a894eeb5e03469d8ce10b20f9702de1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6fba4b375e96ef4032c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "99c381b1d2c56165f419",
+    "url": "/static/css/main.2afcb370.chunk.css"
+  },
+  {
+    "revision": "c6fba4b375e96ef4032c",
+    "url": "/static/js/2.e6a2e2b0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e6a2e2b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c381b1d2c56165f419",
+    "url": "/static/js/main.b2bed38f.chunk.js"
+  },
+  {
+    "revision": "b3aa5d66692a8070b503",
+    "url": "/static/js/runtime-main.0ec948c1.js"
+  }
+]);
